0b8d82f945c4475fd1137156b1eb3549c9ecc9be
Levy Kohout <LevyKohout@Levy-Kohout.local>
2016-09-26 15:34:48 -0500

    final answer

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -3,8 +3,16 @@
     value of checkz by 1.
 */
 var checkz = 0;
+for (var i=0; i <5; i++){
+  checkz+=1;
+}
 
 /*
 2 - Write a while loop that runs 3 times. Each iteration will decrease
     the value of checkz by 2
 */
+var j=0;
+while (j < 3 ){
+  checkz = checkz - 2;
+  j++;
+}
--- 3-conditionals/if.js
+++ 3-conditionals/if.js
@@ -4,11 +4,24 @@
 var first = 3;
 var second = 5;
 
-// 2 - Expand your answer above to console log "Goodbye" if the variables are not equal
+if (first == second){
+  console.log("Hello");
+}
 
+// 2 - Expand your answer above to console log "Goodbye" if the variables are not equal
+else if (first != second) {
+  console.log("Goodbye");
+}
 
 /*
 3 - Write a statement that assigns the value of the addition of the first & second variables
     to a new variable called third IF first is greater than second. If second is greater than
     first assign the difference of the first & second variables to third.
 */
+var third;
+if(first>second){
+  third = first + second;
+}
+if (second>first){
+  third = second - first;
+}
